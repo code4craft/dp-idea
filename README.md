d18883e8615764e7b3daf3229c38b6602cb0f143
yihua.huang <code4crafter@gmail.com>
2014-02-28 16:49:50 +0800

    fix path

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
 DP-IDEA
 =====
 
+为点评中间件定制的Intellij插件！
+
+## 安装：
+
+下载[http://code.dianpingoa.com/yihua.huang/dp-idea/raw/master/dp-idea.zip](http://code.dianpingoa.com/yihua.huang/dp-idea/raw/master/dp-idea.zip)到本地，无需解压。
+
+在Intellij里选择`Settings`=>`Plugins`=>`Install Plugin From Disk`即可。
+
+安装后，点击工具栏的小黄人即可启动。
+
+![]()
+
 ## 功能：
 
 ### mocksocks
 
-如何增加启动参数
+对本地的网络请求进行抓包和调试。从此连接服务器不对、bug不好排查的问题都能迎刃而解了！
 
-[http://confluence.jetbrains.com/display/IDEADEV/Run+Configurations](http://confluence.jetbrains.com/display/IDEADEV/Run+Configurations)
+如何使用？
+
+点击小黄人启动mocksocks，然后启动你的应用程序，**无需做任何配置**，你的应用产生的网络数据都已经被记录下来了！
+
+
+
+### todo
 
 * 查看lion配置
 * 清除alpha和beta缓存
--- src/com/dianping/idea/mocksocks/MocksocksRunPatcher.java
+++ src/com/dianping/idea/mocksocks/MocksocksRunPatcher.java
@@ -5,6 +5,8 @@ import com.intellij.execution.configurations.JavaParameters;
 import com.intellij.execution.configurations.ParametersList;
 import com.intellij.execution.configurations.RunProfile;
 import com.intellij.execution.runners.JavaProgramPatcher;
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginManager;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
@@ -19,7 +21,8 @@ public class MocksocksRunPatcher extends JavaProgramPatcher {
     @Override
     public void patchJavaParameters(Executor executor, RunProfile configuration, JavaParameters javaParameters) {
         if (mocksocksConfig.isProxyOn()) {
-            String clientJarPath = StringUtils.join(new String[]{System.getProperty("idea.plugins.path"), "dp-idea", "lib", "mocksocks-client.jar"}, File.separator);
+            IdeaPluginDescriptor plugin = PluginManager.getPlugin(PluginManager.getPluginByClassName(getClass().getName()));
+            String clientJarPath = StringUtils.join(new String[]{plugin.getPath().getAbsolutePath(), "lib", "mocksocks-client.jar"}, File.separator);
             ParametersList vmParametersList = javaParameters.getVMParametersList();
             vmParametersList
                     .add("-javaagent:" + clientJarPath);
